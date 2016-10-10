cf7d450bbf8950f253972459fb5e78914362ddbe
docodo19 <docodo19@gmail.com>
2016-10-10 09:20:22 -0700

    initial

---
+++ ngApp/controllers/controllers.ts
@@ -0,0 +1,17 @@
+namespace cardealership.Controllers {
+
+    export class HomeController {
+        public message = 'Hello from the home page!';
+        public movies;
+
+        constructor(movieService:cardealership.Services.MovieService) {
+            this.movies = movieService.listMovies();
+        }
+    }
+
+
+    export class AboutController {
+        public message = 'Hello from the about page!';
+    }
+
+}
